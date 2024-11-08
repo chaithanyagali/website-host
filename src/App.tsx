8446cae44da691aca94238f6dc5fd555c6b384c6
chaithanyagali <Chaithanyagali2001@gmail.com>
2024-11-09 00:13:24 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,9 @@ function App() {
       <div className="min-h-screen bg-white">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/properties" element={<Properties />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/website-host/" element={<Hero />} />
+          <Route path="/website-host/properties" element={<Properties />} />
+          <Route path="/website-host/contact" element={<Contact />} />
         </Routes>
         <footer className="bg-gray-900 text-white py-12">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
